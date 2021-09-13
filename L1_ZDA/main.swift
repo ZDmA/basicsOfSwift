bb3e168fadf04c167795108153cca9f1a18d3730
Dmitriy <main-zd@mail.ru>
2021-09-14 01:04:27 +0300

    Скорректировал по замечаниям

--- L1_ZDA/main.swift
+++ L1_ZDA/main.swift
@@ -10,37 +10,33 @@ import Foundation
 // MARK: - ЗАДАНИЕ 1
 print ("Решение задания №1: ==================================================")
 // Задание №1:  Решить квадратное уравнение.
-// Формат квадратнго уравления ax^2 + bx + c = 0
+// Формат квадратного уравления ax^2 + bx + c = 0
 // Задаем коэффициенты квадратного уравлнения КОНСТАНТАМИ
-let a : Float32 = 2.0
-let b : Float32 = 5
-let c : Float32 = 1
+let a: Float32 = 2.0
+let b: Float32 = 5
+let c: Float32 = 1
 
 print("Общий вид квадратного уравнения: ", a, "x^2 + ", b, "x + ", c, "= 0")
       
 //Находим ДИСКРИМИНАНТ
-let Discriminant:Float32 = powf(b, 2) - 4 * a * c
+let discriminant: Float32 = powf(b, 2) - 4 * a * c
 
-print("Дискриминант =", Discriminant)
+print("Дискриминант =", discriminant)
 
 // Определяем наличие корней уравнения
-if Discriminant < 0 {
+if discriminant < 0 {
     print("Квадратное уравнение - не имеет корней")
-}
-// Квадратное уравнение с заданными коэффициентами имеет 1 корень
-else if Discriminant == 0 {
+} else if discriminant == 0 {
     print("Квадратное уравнение - имеет 1 корень")
 
-    let x1 : Float32 = (-b + sqrtf(Discriminant)) / (2 * a)
+    let x1: Float32 = (-b + sqrtf(discriminant)) / (2 * a)
     
     print("x1 =", x1)
-}
-// Discriminant > 0 - Квадратное уравнение с заданными коэффициентами имеет 2 корня
-else {
+} else { // Квадратное уравнение с заданными коэффициентами имеет 2 корня
     print("Квадратное уравнение - имеет 2 корня")
     
-    let x1 : Float32 = (-b + sqrtf(Discriminant)) / (2 * a)
-    let x2 : Float32 = (-b - sqrtf(Discriminant)) / (2 * a)
+    let x1: Float32 = (-b + sqrtf(discriminant)) / (2 * a)
+    let x2: Float32 = (-b - sqrtf(discriminant)) / (2 * a)
     
     print("x1 =", x1)
     print("x2 =", x2)
@@ -49,41 +45,40 @@ else {
 //MARK: ЗАДАНИЕ 2
 print("\nРешение задания №2: ==================================================")
 // Задание №2: Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.
-// Катеты прямоугольнго треугольника обозначим - А и В
-let katetA : Float32 = 7.12
-let katetB : Float32 = 4.6
-
+// Катеты прямоугольнго треугольника обозначим - katetА и katetВ
+let katetA: Float32 = 7.12
+let katetB: Float32 = 4.6
 print("Заданы катеты прямоугольного треугольника: a =", katetA, "b =", katetB)
 
 // Вычисление площади прямоугольного треугольника (s)
-let s : Float32 = (katetA * katetB) / 2
+let s: Float32 = (katetA * katetB) / 2
 print("Площадь треугольника s =", s)
 
 // Гипотенузу прямоугольного треугольника обозначим - С
-let gipC : Float32 = sqrtf(powf(katetA, 2) + powf(katetB, 2))
+let gipC: Float32 = sqrtf(powf(katetA, 2) + powf(katetB, 2))
 print("Гипотенуза c =", gipC)
 
 // Вычисление периметра прямоугольного треугольника (P)
-let perimetr : Float32 = katetA + katetB + gipC
+let perimetr: Float32 = katetA + katetB + gipC
 print("Периметр треугольника p =", perimetr)
 
 //MARK: - ЗАДНИЕ 3
 print("\nРешение задания №3: ===================================================")
 // Задание №3: Пользователь вводит сумму вклада в банк и годовой процент. Найти сумму вклада через 5 лет.
 
-var sumUser : Int64 = 0
-var persentInYear : Double = 0.0
-var sumDeposit : Double = 0.0
-var stringSum : String? = ""
-var stringPers : String? = ""
-let years : Int16 = 5
+var sumUser: Int64 = 0
+var persentInYear: Double = 0.0
+var sumDeposit: Double = 0.0
+var stringSum: String? = ""
+var stringPers: String? = ""
+let years: Int16 = 5
 
 print ("Введите сумму вклада в руб. (целое число): ")
 stringSum = readLine()
 print ("Введите годовой процент по вкладу (разделитель '.'): ")
 stringPers = readLine()
 
-if stringSum != nil && stringPers != nil {
+if stringSum != nil, stringPers != nil {
     let strSum = stringSum!
     let strPers = stringPers!
     
@@ -93,17 +88,15 @@ if stringSum != nil && stringPers != nil {
         persentInYear = persDoub / 100
     
         for _ in 1...years {
-            let OneYearMoney = (sumDeposit * persentInYear)
-            sumDeposit += OneYearMoney
+            let oneYearMoney = (sumDeposit * persentInYear)
+            sumDeposit += oneYearMoney
         }
         
         print("При вкладе ", sumUser, " руб. сроком на 5 лет ваши накопления увеличатся до суммы: ", Int(sumDeposit), "руб.")
+    } else {
+        print("Введены некоректные числа, запустите программу заново '\(strSum)', '\(strPers)'")
     }
-    else {
-        print("Введены некоректные числа, запустите программу заново.")
-    }
-}
-else {
+} else {
   print("Вы не ввели данные, запустите программу заново.")
 }
 
